21c65665b4ae0b1d02ab72bd26ff0053fb0ec170
Sachin Basnet <sbasnet@uef.fi>
2024-06-04 16:58:13 +0300

    tailwind css configuration

--- Front-End Site/src/App.jsx
+++ Front-End Site/src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <h1>Welcome! Website under construction.</h1>
+      <h1 className='text-xl font-bold text-red-500'>Welcome! Website is under construction.</h1>
     </>
   )
 }
